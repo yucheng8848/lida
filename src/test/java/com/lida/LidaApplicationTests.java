cbcfdec121c4e8d9e29729c34e581c4c4b61e0e7
yucheng <2729000275@qq.com>
2023-03-16 22:42:40 +0800

    2.0

--- src/test/java/com/lida/LidaApplicationTests.java
+++ src/test/java/com/lida/LidaApplicationTests.java
@@ -1,6 +1,5 @@
 package com.lida;
 
-import com.lida.mapper.CustomersMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/lida/MybaitsPlusTest.java
@@ -0,0 +1,20 @@
+package com.lida;
+
+import com.lida.entity.Customers;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MybaitsPlusTest {
+
+//    @Autowired
+//    private CustomersMapper customersMapper;
+//
+//    @Test
+//    public void testSelectList(){
+//        //通过条件构造器查询list
+//        List<Customers> list = customersMapper.selectList(null);
+//        list.forEach(System.out::println);
+//    }
+}
