c7ff73a5675d9fe48f7b62496cdd27007e804833
yucheng <2729000275@qq.com>
2023-04-05 18:04:13 +0800

    6.0

--- src/main/java/com/lida/LidaApplication.java
+++ src/main/java/com/lida/LidaApplication.java
@@ -3,6 +3,7 @@ package com.lida;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
--- src/test/java/com/lida/MybaitsPlusTest.java
+++ src/test/java/com/lida/MybaitsPlusTest.java
@@ -1,8 +1,11 @@
 package com.lida;
 
+import com.lida.controller.LikesController;
 import com.lida.entity.Businesses;
 import com.lida.entity.Customers;
+import com.lida.entity.Likes;
 import com.lida.mapper.BusinessesMapper;
+import com.lida.mapper.LikesMapper;
 import com.lida.mapper.ValuationMapper;
 import com.lida.vo.BusinessesVo;
 import com.lida.vo.OrdersVo;
@@ -10,24 +13,36 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 public class MybaitsPlusTest {
 
-    @Autowired
+    @Resource
     private ValuationMapper valuationMapper;
 
-    @Autowired
+    @Resource
     private BusinessesMapper businessesMapper;
+
+    @Resource
+    private LikesController likesController;
+
+    @Resource
+    private LikesMapper likesMapper;
 
     @Test
     public void testSelectList(){
-
+        likesMapper.hhhh();
+//        Likes likes = new Likes();
+//        likes.setCustomerId(1);
+//        likes.setBusinessesId(1);
+//
+//        likesController.update(likes);
         //通过条件构造器查询list
-        BusinessesVo businessesVo = businessesMapper.selectAllById(3);
+//        BusinessesVo businessesVo = businessesMapper.selectAllById(3);
 //        list.forEach(System.out::println);
-        System.out.println(businessesVo);
+//        System.out.println(businessesVo);
     }
 }
